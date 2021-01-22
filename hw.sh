7429e0e240daf5be6bfbe9b7edf098e62cfdf112
Kisiga Timothy Mukodo <ktimo550@gmail.com>
2021-01-22 09:21:46 +0300

    first batch script

---
+++ hw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#: Title       : hw
+#: Date        :2021-01-22
+#: Author      : "Kisiga Timothy Mukodo"
+#: Version     : 1.0.1
+#: Description : print hello world
+#: Options     : None
+
+
+printf "%s\n" "Hello, World!"
